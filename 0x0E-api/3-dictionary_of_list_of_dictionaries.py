769eac4a419ec55d71eeb356da151148efab8848
Dustin Davis <davisdustin328@gmail.com>
2023-04-03 16:05:09 -0500

    built on from 2, to Get all tasks from all employees

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Gathers data from an API endpoint and exports it in JSON format
+"""
+
+import json
+import requests
+
+
+if __name__ == "__main__":
+    # Define endpoint
+    url = "https://jsonplaceholder.typicode.com/todos"
+    
+    # Send request to API and parse response
+    response = requests.get(url)
+    tasks = response.json()
+
+    # Format data as JSON
+    data = {}
+    for task in tasks:
+        user_id = task['userId']
+        if user_id not in data:
+            data[user_id] = []
+        data[user_id].append({
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": ""
+        })
+
+    # Get user names
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url)
+    users = response.json()
+    for user in users:
+        user_id = user['id']
+        if user_id in data:
+            for task in data[user_id]:
+                task['username'] = user['username']
+
+    # Write data to JSON file
+    filename = "todo_all_employees.json"
+    with open(filename, mode='w') as file:
+        json.dump(data, file)
